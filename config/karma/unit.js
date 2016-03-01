b24d85e57b14090a4080659165de987601e37177
Christoph Guttandin <chrisguttandin@media-codings.com>
2016-03-01 13:54:44 +0100

    disable tests with FirefoxDeveloper on SauceLabs

--- config/karma/unit.js
+++ config/karma/unit.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
         configuration.browsers = [
             'ChromeCanarySauceLabs',
             'ChromeSauceLabs',
-            'FirefoxDeveloperSauceLabs',
+            // 'FirefoxDeveloperSauceLabs',
             'FirefoxSauceLabs'
         ];
 
