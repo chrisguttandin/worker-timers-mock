a8d8516c78571e08754addaed8db968786d0ab37
chrisguttandin <chrisguttandin@media-codings.com>
2017-06-05 16:59:03 +0200

    use vehicles for scheduling

--- src/module.ts
+++ src/module.ts
@@ -1,29 +1,18 @@
 import { SinonStub, stub } from 'sinon';
+import { DeLorean } from 'vehicles';
 
-let currentIntervalTime = 0;
-let lastIntervalId = -1;
-
-const scheduledIntervalFunctions: { delay: number, func: Function, id: number, nextTime: number }[] = [];
+const deLorean = new DeLorean();
+const idToTicketMap = new Map();
 
 const workerTimers = {
     clearInterval () {}, // tslint:disable-line:no-empty
     clearTimeout () {}, // tslint:disable-line:no-empty
     flushInterval (elapsedTime: number) {
-        currentIntervalTime += elapsedTime;
-
-        while (scheduledIntervalFunctions.length && scheduledIntervalFunctions[0].nextTime <= currentIntervalTime) {
-            const scheduledIntervalFunction = scheduledIntervalFunctions[0];
-
-            scheduledIntervalFunction.func();
-            scheduledIntervalFunction.nextTime += scheduledIntervalFunction.delay;
-
-            scheduledIntervalFunctions.sort((a, b) => a.nextTime - b.nextTime);
-        }
+        deLorean.travel(elapsedTime);
     },
     resetInterval () {
-        currentIntervalTime = 0;
-        lastIntervalId = -1;
-        scheduledIntervalFunctions.length = 0;
+        deLorean.reset();
+        idToTicketMap.clear();
 
         (<SinonStub> workerTimers.clearInterval).reset();
         (<SinonStub> workerTimers.setInterval).reset();
@@ -33,15 +22,8 @@ const workerTimers = {
 };
 
 stub(workerTimers, 'clearInterval').callsFake((id) => {
-    scheduledIntervalFunctions.some((scheduledIntervalFunction) => {
-        const found = (scheduledIntervalFunction.id === id);
-
-        if (found) {
-            scheduledIntervalFunctions.splice(scheduledIntervalFunctions.indexOf(scheduledIntervalFunction), 1);
-        }
-
-        return found;
-    });
+    deLorean.cancel(id);
+    idToTicketMap.delete(id);
 });
 
 stub(workerTimers, 'clearTimeout').callsFake(() => { // tslint:disable-line:no-empty
@@ -49,18 +31,13 @@ stub(workerTimers, 'clearTimeout').callsFake(() => { // tslint:disable-line:no-e
 });
 
 stub(workerTimers, 'setInterval').callsFake((func, delay) => {
-    lastIntervalId += 1;
+    const id = deLorean.schedule(deLorean.position + delay, function funcWithScheduler() {
+        idToTicketMap.set(id, deLorean.schedule(deLorean.position + delay, funcWithScheduler));
 
-    const id = lastIntervalId;
-
-    scheduledIntervalFunctions.push({
-        delay,
-        func,
-        id,
-        nextTime: currentIntervalTime + delay
+        func();
     });
 
-    scheduledIntervalFunctions.sort((a, b) => a.nextTime - b.nextTime);
+    idToTicketMap.set(id, id);
 
     return id;
 });
