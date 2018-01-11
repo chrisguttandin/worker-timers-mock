471ff9ce0835efd68f3a15a31a5c687f458163bd
chrisguttandin <chrisguttandin@media-codings.com>
2018-01-11 22:08:15 +0100

    style: apply linting rules

--- src/module.ts
+++ src/module.ts
@@ -4,40 +4,44 @@ import { DeLorean, IVehicle } from 'vehicles';
 const deLorean = new DeLorean();
 const intervalIdToTicketMap: Map<number, number> = new Map();
 
-export const clearInterval: SinonStub = stub().callsFake((id) => {
-    const ticket = intervalIdToTicketMap.get(id);
+export const clearInterval: SinonStub = stub()
+    .callsFake((id) => {
+        const ticket = intervalIdToTicketMap.get(id);
 
-    if (ticket !== undefined) {
-        deLorean.cancel(ticket);
-    }
+        if (ticket !== undefined) {
+            deLorean.cancel(ticket);
+        }
 
-    intervalIdToTicketMap.delete(id);
-});
+        intervalIdToTicketMap.delete(id);
+    });
 
-export const clearTimeout: SinonStub = stub().callsFake((id) => {
-    deLorean.cancel(id);
-    intervalIdToTicketMap.delete(id);
-});
+export const clearTimeout: SinonStub = stub()
+    .callsFake((id) => {
+        deLorean.cancel(id);
+        intervalIdToTicketMap.delete(id);
+    });
 
 export const getVehicle = (): IVehicle => {
     return deLorean;
 };
 
-export const setInterval: SinonStub = stub().callsFake((func, delay) => {
-    const id = deLorean.schedule(deLorean.position + delay, function funcWithScheduler () {
-        intervalIdToTicketMap.set(id, deLorean.schedule(deLorean.position + delay, funcWithScheduler));
+export const setInterval: SinonStub = stub()
+    .callsFake((func, delay) => {
+        const id = deLorean.schedule(deLorean.position + delay, function funcWithScheduler () {
+            intervalIdToTicketMap.set(id, deLorean.schedule(deLorean.position + delay, funcWithScheduler));
 
-        func();
+            func();
+        });
+
+        intervalIdToTicketMap.set(id, id);
+
+        return id;
     });
 
-    intervalIdToTicketMap.set(id, id);
-
-    return id;
-});
-
-export const setTimeout: SinonStub = stub().callsFake((func, delay) => {
-    return deLorean.schedule(deLorean.position + delay, func);
-});
+export const setTimeout: SinonStub = stub()
+    .callsFake((func, delay) => {
+        return deLorean.schedule(deLorean.position + delay, func);
+    });
 
 export const reset = () => {
     deLorean.reset();
